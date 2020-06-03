473836e75fae9ff017ae887a39cd925592af395b
China Yu <china.yu@kodakollectiv.com>
2020-06-03 22:31:11 +0800

    Add icon to categories and updated db schema and seed
    Signed-off-by: China Yu <china.yu@kodakollectiv.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_03_104931) do
+ActiveRecord::Schema.define(version: 2020_06_03_142840) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2020_06_03_104931) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "icon"
   end
 
   create_table "records", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,27 +1,22 @@
 categories = [
-  "Food & Drinks",
-  "Groceries",
-  "Clothes",
-  "Electronics",
-  "Medicine",
-  "Pets",
-  "Kids",
-  "Gifts",
-  "Transportation",
-  "Books",
-  "Education",
-  "Hobbies",
-  "Music",
-  "Wellness",
-  "Subscriptions",
-  "Internet",
-  "Phone",
-  "Software",
-  "Games"
+  { name: "Food & Drinks", icon: "/images/ic_food_drinks.png" },
+  { name: "Groceries", icon: "/images/ic_groceries.png" },
+  { name: "Clothes", icon: "/images/ic_clothes.png" },
+  { name: "Electronics", icon: "/images/ic_electronics.png" },
+  { name: "Healthcare", icon: "/images/ic_healthcare.png" },
+  { name: "Gifts", icon: "/images/ic_gifts.png" },
+  { name: "Transportation", icon: "/images/ic_transportation.png" },
+  { name: "Education", icon: "/images/ic_education.png" },
+  { name: "Entertainment", icon: "/images/ic_entertainment.png" },
+  { name: "Utilities", icon: "/images/ic_utilities.png" },
+  { name: "Rent", icon: "/images/ic_rent.png" },
+  { name: "Household Supplies", icon: "/images/ic_household.png" },
+  { name: "Investments", icon: "/images/ic_investments.png" },
+  { name: "Other", icon: "/images/ic_other.png" }
 ]
 
 if Category.count == 0
   categories.each do |cat|
-    Category.create(name: cat)
+    Category.create(name: cat[:name], icon: cat[:icon])
   end
 end
